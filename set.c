4531e79dfa1a683969596321cead80826c8ed893
Chloe <chloe26.parisse@gmail.com>
2023-10-26 15:42:08 +0200

    set.c et set.h

--- set.c
+++ set.c
@@ -2,4 +2,70 @@
 // Created by Hawa Ajroud on 26/10/2023.
 //
 
+
+///////level cells
+// Create_cell
+// param :value, nb_levels
+// output : pointer to the cell
+t_d_cell *Create_cell(int val, int nb)
+{
+    t_d_cell* cell;
+    cell->value = val;
+    cell->nb_levels = nb;
+    cell->next = NULL;
+    //cell->down ?? not sure if we implement it since we have nb
+    return cell;
+}
+
+///////level lists
+// Create_empty_lev_list
+// param : int max_nb_levels  (of list)
+// output : level_list
+t_d_list Create_empty_lev_list(int levels)
+{
+    t_d_list list;
+    list.max_level = levels;
+    for (int i = 0; i < levels; i++) {
+        list.head_[i] = NULL; // Initialize level's head to NULL
+    }
+    return list;
+}
+
+//Insert_Cell_Head_list
+// param : t_d_list* list, int  value_cell, int level_cell
+// output : void
+void Insert_Cell_Head_list(t_d_list* list, int  value_cell, int level_cell)
+{
+    t_d_list *new_cell = Create_cell(value_cell, level_cell);
+    for (int i = 0; i<level_cell; i++) {
+        list->head_[i] = new_cell;
+    }
+}
+
+// Display_cells_level
+// param : int level, t_d_list list
+// output : t_d_cell cells
+void Display_cells_level(t_d_list list, int level) {
+    if (level < 0 || level >= list.max_level) {
+        printf("[list_head_%d @-]-->NULL\n", level);
+        return;
+    }
+
+    if (list.head_[level] == NULL) {
+        printf("[list_head_%d @-]-->NULL\n", level);
+    }
+    else
+    {
+        t_d_cell *temp=list.head_[level];
+        printf("[list_head_%d @-]-->",level);
+        while(temp->next != NULL)
+        {
+            printf("-->[ %d|@-]", temp->value);
+            temp = temp->next;
+        }
+        printf("-->NULL\n");
+    }
+}
+
+
 #include "set.h"
--- set.h
+++ set.h
@@ -5,4 +5,62 @@
 #ifndef C_PROJECT_S3_SET_H
 #define C_PROJECT_S3_SET_H
 
+
+
+//Cell
+typedef struct s_d_cell
+{
+    int value, nb_levels;
+    struct s_d_cell *next;
+    struct s_d_cell *down_[nb_levels-1];
+    //struct s_d_cell *down;
+} t_d_cell;
+
+//Level_list
+typedef struct s_d_list
+{
+    t_d_cell *head_[max_level-1];
+    int max_level;
+} t_d_list;
+
+
+///////level cells
+// Create_cell
+// param : int value, int nb_levels
+// output : pointer to the cell
+t_d_cell* Create_cell (int, int);
+
+
+///////level lists
+// Create_empty_lev_list
+// param : int max_nb_levels  (of list)
+// output : level_list
+t_d_list Create_empty_lev_list(int);
+
+
+//Insert_Cell_Head_list
+// param : t_d_list* list, int  value_cell, int level_cell
+// output : void
+void Insert_Cell_Head_list(t_d_list*, int, int);
+
+// Display_cells_level
+// param : int level, t_d_list list
+// output : t_d_cell cells
+void Display_cells_level(t_d_list , int);
+
+// Display_levels_list_align_cells
+// param : int level , t_d_list list
+// output : t_d_cell cells (to align)
+void Display_levels_list_align_cells(t_d_list , int);
+
+// Insert_level_cell
+// param : int level, t_d_cell cell_to_insert
+// output : void
+// ATTENTION TO THE ORDER OF THE INSERTION = MUST STAY A SORTED LIST
+void Insert_level_cell(int, t_d_cell);
+
+//For this last function, it is possible to make a 'simple' insertion at each level, starting
+//from the beginning of the list, but it is possible to be more efficient - it's up to you
+//to work out how.
+
 #endif //C_PROJECT_S3_SET_H
