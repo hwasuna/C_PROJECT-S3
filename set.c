85a8587a8bc6b62e6a09f769c6a9637a9b3d326f
hw_ajrd <hawa.ajr@gmail.com>
2023-12-10 17:39:35 +0100

    update des commentaires de la fonction display_aligned et get_length_int (= list_length)

--- set.c
+++ set.c
@@ -92,64 +92,76 @@ void Display_level(t_d_list* list, int level) {
     }
 }
 
-
-int get_lenght_int(int a)
+int list_length(int x)
 {
-    int add=0;
-    if (a<0)
+    int sum=0;
+    if (x<0)
     {
-        add=1;
-        a=-a;
+        sum=1;
+        x=-x;
     }
-    else if (a==0)
+    else if (x==0)
     {
         return 0;
     }
-    return add+(int)log10((double )a);
+    return sum+(int)log10((double)x);
 }
+
 
 ////// Display_align
 ////// param : t_d_list list
 ////// output : void
-void Display_align(t_d_list *list) // Display level list function, it takes 1 parameter : the address of the level list [my_t_d_list]
+
+void Display_aligned(t_d_list *list) // Display level list function, it takes 1 parameter : the address of the level list [my_t_d_list]
 {
-    if (list->max_level == 0) // Check if the level list is not created ?
+    if (list->max_level == 0)
+//  Check if the level list exist
     {
-        printf("The level list is empty\n"); // Display it's impossible to display a none created level list
+        printf("The level list is empty\n");
+//      The level list doesn't exist => it's impossible
     }
     else
     {
-        t_d_cell *temp_ptr; // Initialize a temporary pointer which points to [t_d_cell] structure
-        for (int i = 0; i < list->max_level; i++)  // Iterate [max_level] times to print all the different levels of the [t_d_list]
+        t_d_cell *temp;
+//      Initialise a temporary pointer to make it point on the cell
+        for (int i = 0; i < list->max_level; i++)
+//      Iterate max_level times to print all the levels of the list
         {
-            temp_ptr = list->head[i]; // temporary pointer get the address of the first byte memory block allocated for [t_d_cell]
-            t_d_cell *temp_shr = list->head[0];
-            printf("[list head_%d @-]", i); // print the different level one by one starting from 0
+            temp = list->head[i];
+//          Pointing the pointer on t_d_cell
+            t_d_cell *temp_ptr = list->head[0];
+            printf("[list head_%d @-]", i);
+//          Print the levels one by one starting from 0
             do
             {
-                while ( temp_ptr != temp_shr)
+                while ( temp != temp_ptr)
+//              As long as the cell's value pointed by 'temp' is not equal to the cell's value pointed by 'temp_ptr', display dashes to align the values
                 {
-                    printf("-----------");
-                    for (int j=0; j<get_lenght_int(temp_shr->value);j++){
+                    printf("-------------");
+                    for (int j=0; j < list_length(temp_ptr->value);j++)
+                    {
                         printf("-");
                     }
-                    temp_shr=temp_shr->next[0];
+                    temp_ptr=temp_ptr->next[0];
                 }
-                printf("--> [ %d|@-]", temp_ptr->value); // display the value of each cell on every level
-                temp_ptr = temp_ptr->next[i];
-                temp_shr = temp_shr->next[0];
-            }
-            while (temp_ptr != NULL); // while the temporary pointer don't point to NULL on each level to display from head to tail
+                printf("----> [ %d|@-]", temp->value);
+//              Display the value of each cell on every level
+                temp = temp->next[i];
+                temp_ptr = temp_ptr->next[0];
+            }while (temp != NULL);
+//          As long as the pointer 'temp' doesn't point on NULL at each level, display from head to tail
 
-            while ( temp_ptr != temp_shr)
+            while (temp != temp_ptr)
             {
-                printf("-----------");
-                for (int j=0; j<get_lenght_int(temp_shr->value);j++){
+                printf("-------------");
+                for (int j=0; j < list_length(temp_ptr->value);j++)
+                {
                     printf("-");
                 }
-                temp_shr=temp_shr->next[0];
+                temp_ptr=temp_ptr->next[0];
             }
-            printf("----> NULL\n"); // display the end of each level
+            printf("----> NULL\n");
+//          Display each level's end
         }
     }
     return;
